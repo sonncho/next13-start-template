d186bdee87d6985bb22ae7c261ee8f80c2500d88
sonncho <dungcho93@gmail.com>
2023-03-19 17:39:48 +0900

    ADD: emotion

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    emotion: true, // default로 설정
+  },
 };
 
 module.exports = nextConfig;
